a723d27f20903faf3549839fda94d85cb46e12ca
Matt Parker <mdparker89@gmail.com>
2015-04-10 17:28:36 -0400

    add chat activity

---
+++ app/src/main/java/io/bitfountain/matthewparker/bitchat/ChatActivity.java
@@ -0,0 +1,64 @@
+package io.bitfountain.matthewparker.bitchat;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class ChatActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        ListView listView = (ListView)findViewById(R.id.messages_list);
+        listView.setAdapter(new MessagesAdapter(new ArrayList<Message>()));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class MessagesAdapter extends ArrayAdapter<Message> {
+        MessagesAdapter(ArrayList<Message> messages){
+            super(ChatActivity.this, R.layout.messages_list_item, R.id.message, messages);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+            Message message = getItem(position);
+
+            TextView nameView = (TextView)convertView.findViewById(R.id.message);
+            nameView.setText(message.getText());
+            return convertView;
+        }
+    }
+}
---
+++ app/src/main/java/io/bitfountain/matthewparker/bitchat/Message.java
@@ -0,0 +1,16 @@
+package io.bitfountain.matthewparker.bitchat;
+
+/**
+ * Created by matthewparker on 4/10/15.
+ */
+public class Message {
+    private String mText;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+}
